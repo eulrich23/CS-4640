1f5cc393c1205b6ea4d03f37e9149baca0bc3a02
Eric <ulrich.eric23@gmail.com>
2020-09-07 13:31:06 -0600

    Tutorials 4-6 Complete
    Completed Tutorials 4-6 and included build under 3D Beginner2

---
+++ 3D Beginner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    bool m_IsPlayeratExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayeratExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayeratExit)
+        {
+            EndLevel();
+        }   
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
